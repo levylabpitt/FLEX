9478fd68da8c057b077ca860a5057594e701fdb0
AriaHajikhani <147864762+AriaHajikhani@users.noreply.github.com>
2025-01-28 18:09:29 -0500

    Update KH7008.py
    adding functions:
    set_all_channels
    get_all_channels
    set_channel
    get_channel

--- src/flex/inst/DAQ/KH7008.py
+++ src/flex/inst/DAQ/KH7008.py
@@ -4,6 +4,7 @@ Levylab FLEX instrument driver for Krohn-Hite 7008.
 
 Authors: 
 Pubudu Wijesinghe <pubudu.wijesinghe@levylab.org>
+Aria Hajikhani <aria.hajikhani@levylab.org>
 
 Contact an author for any queries.
 '''
@@ -20,6 +21,29 @@ os.makedirs(logpath, exist_ok=True)
 class KH7008(Instrument):
     def __init__(self, address= _DEFAULT_ADDRESS):
         super().__init__(address, log_file= logpath + '\instrument.log')
+    
+    def set_all_channels(self, channels_config):
+        cmd = 'setAllChannels'
+        params = {'params': channels_config}
+        response = self._send_command(cmd, params)
+        return response.get('result')
+    
+    def get_all_channels(self):
+        cmd = 'getAllChannels'
+        response = self._send_command(cmd)
+        return response.get('result')
+    
+    def set_channel(self, channel_config):
+        cmd = 'setChannel'
+        params = {'params': channel_config}
+        response = self._send_command(cmd, params)
+        return response.get('result')
+    
+    def get_channel(self, channel):
+        cmd = 'getChannel'
+        params = {'channel': channel}
+        response = self._send_command(cmd, params)
+        return response.get('result')
     
 if __name__ == "__main__":
     # Test the KH7008 class

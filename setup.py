ce299914cd0f4fc4d0dd28445a67c84cfd479783
PubuduW - CATSEYE <pubudu.wijesinghe@levylab.org>
2026-03-11 20:51:04 -0400

    chore: bump version to 1.0.0 for initial release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flex',
-    version='0.0.1',
+    version='1.0.0',
     description='Framework for Laboratory EXperiments',
     author='Pubudu Wijesinghe',
     author_email='pubudu.wijesinghe@levylab.org',
